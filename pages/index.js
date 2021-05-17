144abecf8d64c50ffac7cc96c392ec7c8cd1a7b3
Chris Miller <miller.t.chris@gmail.com>
2021-05-16 21:48:44 -0400

    An extremely crude example of centering a <div>

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
+  function justifyContent(e) {
+    var position = e.target.getAttribute("data-style");
+    document.querySelector(".scene").style.justifyContent = position;
+  }
+
+  function alignItems(e) {
+    var position = e.target.getAttribute("data-style");
+    document.querySelector(".scene").style.alignItems = position;
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className="site-wrap">
+        <aside className="panel">
+          <h1>Controls</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <div className="btn-group horizontal">
+            <h2>Horizontal</h2>
+            <button
+              className="btn"
+              data-style="flex-start"
+              onClick={justifyContent}
+            >
+              Left
+            </button>
+            <button
+              className="btn"
+              data-style="center"
+              onClick={justifyContent}
+            >
+              Center
+            </button>
+            <button
+              className="btn"
+              data-style="flex-end"
+              onClick={justifyContent}
+            >
+              Right
+            </button>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <div className="btn-group vertical">
+            <h2>Horizontal</h2>
+            <button
+              className="btn"
+              data-style="flex-start"
+              onClick={alignItems}
+            >
+              Left
+            </button>
+            <button className="btn" data-style="center" onClick={alignItems}>
+              Center
+            </button>
+            <button className="btn" data-style="flex-end" onClick={alignItems}>
+              Right
+            </button>
+          </div>
+        </aside>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <main className="scene">
+          <div className="shape"></div>
+        </main>
+      </div>
+    </>
+  );
 }
